c8e84ec56fc3d941284b33f77fe7144e00182e36
hyeong gil <gudrif7913@naver.com>
2020-01-06 07:58:42 +0900

    컴포넌트에 store연결

--- timerAppProject/App.js
+++ timerAppProject/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Timer from './component/Timer';
+import reducer from './reducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+let store = createStore(reducer);
 
 export default function App() {
   return (
-    <Timer />
+    <Provider store={store}>
+      <Timer />
+    </Provider>    
   );
 }
 
