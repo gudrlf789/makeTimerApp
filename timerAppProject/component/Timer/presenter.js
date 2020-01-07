d0f347f8ad89c60cc01df3cc7d589af144a9f883
hyeong gil <gudrif7913@naver.com>
2020-01-07 19:29:47 +0900

    초를 분으로 변환 작업

--- timerAppProject/component/Timer/presenter.js
+++ timerAppProject/component/Timer/presenter.js
@@ -2,14 +2,39 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import Button from '../Button';
 
+function formatTime(time){
+    let minutes = Math.floor(time/60);
+    time  -= minutes * 60;
+    let seconds = parseInt(time % 60, 10);
+    return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
 class Timer extends Component {
+
+    componentWillReceiveProps(nextProps){
+        const currentProps = this.props;
+        if(!currentProps.isPlaying && nextProps.isPlaying){
+            // 인터벌 함수 시작
+            const timerInterval = setInterval(()=>{
+                currentProps.addSecond();
+            }, 1000);
+            this.setState({
+                timerInterval
+            })
+
+        }else if(currentProps.isPlaying && !nextProps.isPlaying){
+            // 인터벌 함수 stop
+            clearInterval(this.state.timerInterval);
+        }
+    }
+
     render(){
-        const { isPlaying, elapsedTime, timerDuration, startTimer, restartTimer } = this.props;
+        const { isPlaying, elapsedTime, timerDuration, startTimer, restartTimer, addSecond } = this.props;
         return(
             <View style={styles.container}>
                 <StatusBar barStyle={"light-content"} />
                 <View style={styles.upper}>
-                    <Text style={styles.time}>25:00</Text>
+                    <Text style={styles.time}>{formatTime(timerDuration - elapsedTime)}</Text>
                 </View>
                 <View style={styles.lower}>
                     {!isPlaying ? (
